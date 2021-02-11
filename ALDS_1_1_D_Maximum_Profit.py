2e54441dae1a9b03d421159da487d024854f6ff5
hsakakur <hide_bsb920@yahoo.co.jp>
2021-02-11 13:16:30 +0900

    mod

--- ALDS_1_1_D_Maximum_Profit.py
+++ ALDS_1_1_D_Maximum_Profit.py
@@ -1,24 +1,24 @@
 import sys
 
-integer_n = int(input())
-if not (2 <= int(integer_n) <= 200000):
-    print('Please integer n is 2 or more and y 20000 less.', sys.stderr)
+n = int(input())
+if not (2 <= int(n) <= 200000):
+    print('Please integer n is 2 or more and 20000 less.', sys.stderr)
     exit(1)
 
-integer_r = list()
-for i in range(integer_n):
+r_list = list()
+for i in range(n):
     r = int(input())
     if not (1 <= r <= 10 ** 9):
-        print('Please integer r is 1 or more and y 10**9 less.', sys.stderr)
+        print('Please integer r is 1 or more and 10**9 less.', sys.stderr)
         exit(1)
-    integer_r.append(r)
+    r_list.append(r)
 
 maximumBenefit = -sys.maxsize
 minimumValue = sys.maxsize
-for i in range(len(integer_r)):
-    if minimumValue > integer_r[i]:
-        minimumValue = integer_r[i]
-    benefit = integer_r[i] - minimumValue
+for i in range(len(r_list)):
+    if minimumValue > r_list[i]:
+        minimumValue = r_list[i]
+    benefit = r_list[i] - minimumValue
     if maximumBenefit < benefit:
         maximumBenefit = benefit
 
